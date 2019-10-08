b988d3316aae6b718bf4f14a2b6a38dc3a6a78f6
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-08 16:10:13 +0100

    Add a questions resource

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { Admin } from "react-admin";
+import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import questions from "./resources/questions";
 
-const dataProvider = jsonServerProvider("http://jsonplaceholder.typicode.com");
-const App = () => <Admin dataProvider={dataProvider} />;
+const dataProvider = jsonServerProvider("http://localhost:3000");
+
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource {...questions} />
+  </Admin>
+);
 
 export default App;
