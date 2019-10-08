60afb420b204742a62e48e012b81d29f7af3d395
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-08 16:20:24 +0100

    Add resources for all models

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
 import React from "react";
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+
 import questions from "./resources/questions";
+import activities from "./resources/activities";
+import completion_questions from "./resources/completion_questions";
+import default_activities from "./resources/default_activities";
+import default_expected_values from "./resources/default_expected_values";
+import default_questions from "./resources/default_questions";
+import expected_values from "./resources/expected_values";
+import follow_up_activities from "./resources/follow_up_activities";
+import involvements from "./resources/involvements";
+import issues from "./resources/issues";
+import multi_choice_options from "./resources/multi_choice_options";
+import programmes from "./resources/programmes";
+import project_activities from "./resources/project_activities";
+import project_questions from "./resources/project_questions";
+import project_summaries from "./resources/project_summaries";
+import project_types from "./resources/project_types";
+import projects from "./resources/projects";
+import resolutions from "./resources/resolutions";
+import responses from "./resources/responses";
+import roles from "./resources/roles";
+import source_materials from "./resources/source_materials";
+import topics from "./resources/topics";
+import user_roles from "./resources/user_roles";
+import users from "./resources/users";
+import visibilities from "./resources/visibilities";
 
 const dataProvider = jsonServerProvider("http://localhost:3000");
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource {...questions} />
+    <Resource {...activities} />
+    <Resource {...completion_questions} />
+    <Resource {...default_activities} />
+    <Resource {...default_expected_values} />
+    <Resource {...default_questions} />
+    <Resource {...expected_values} />
+    <Resource {...follow_up_activities} />
+    <Resource {...involvements} />
+    <Resource {...issues} />
+    <Resource {...multi_choice_options} />
+    <Resource {...programmes} />
+    <Resource {...project_activities} />
+    <Resource {...project_questions} />
+    <Resource {...project_summaries} />
+    <Resource {...project_types} />
+    <Resource {...projects} />
+    <Resource {...questions} />
+    <Resource {...resolutions} />
+    <Resource {...responses} />
+    <Resource {...roles} />
+    <Resource {...source_materials} />
+    <Resource {...topics} />
+    <Resource {...user_roles} />
+    <Resource {...users} />
+    <Resource {...visibilities} />
   </Admin>
 );
 
---
+++ src/resources/resolutions.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Create, Datagrid, DateField, DisabledInput, Edit, List, SimpleForm, TextField } from "react-admin";
+
+const form = (
+  <SimpleForm>
+    <DisabledInput source="id" />
+    <DisabledInput source="created_at" />
+    <DisabledInput source="updated_at" />
+  </SimpleForm>
+);
+
+export default {
+  name: "resolutions",
+  edit: (props) => <Edit {...props}>{form}</Edit>,
+  create: (props) => <Create {...props}>{form}</Create>,
+  list: (props) => (
+    <List {...props}>
+      <Datagrid rowClick="edit">
+        <TextField source="id" />
+        <DateField source="created_at" />
+        <DateField source="updated_at" />
+      </Datagrid>
+    </List>
+  ),
+};
