5ff895b289501735ff6d1d301f9ec54ca100eab1
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-09 16:32:54 +0100

    Add create and list views for project summaries

--- src/resources/project_summaries.js
+++ src/resources/project_summaries.js
@@ -1,24 +1,43 @@
 import React from "react";
-import { Create, Datagrid, DateField, DisabledInput, Edit, List, SimpleForm, TextField } from "react-admin";
+import { Create, Datagrid, DateField, Edit, List, SimpleForm, TextField } from "react-admin";
+import { LongTextInput, ReferenceInput, SelectInput, ReferenceField } from "react-admin";
 
-const form = (
-  <SimpleForm>
-    <DisabledInput source="id" />
-    <DisabledInput source="created_at" />
-    <DisabledInput source="updated_at" />
-  </SimpleForm>
-);
+const form = ({ location }) => {
+  const newRecord = location.pathname.match(/create/);
+
+  return (
+    <SimpleForm>
+      {newRecord ? null : <TextField source="id" />}
+
+      <ReferenceInput source="project_id" reference="projects">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+
+      <LongTextInput source="text" />
+
+      {newRecord ? null : <TextField source="created_at" />}
+      {newRecord ? null : <TextField source="updated_at" />}
+    </SimpleForm>
+  );
+};
 
 export default {
   name: "project_summaries",
-  edit: (props) => <Edit {...props}>{form}</Edit>,
-  create: (props) => <Create {...props}>{form}</Create>,
+  edit: (props) => <Edit {...props}>{form(props)}</Edit>,
+  create: (props) => <Create {...props}>{form(props)}</Create>,
   list: (props) => (
     <List {...props}>
       <Datagrid rowClick="edit">
         <TextField source="id" />
-        <DateField source="created_at" />
-        <DateField source="updated_at" />
+
+        <ReferenceField source="project_id" reference="projects">
+          <TextField source="name" />
+        </ReferenceField>
+
+        <TextField source="text" />
+
+        <DateField source="created_at" showTime />
+        <DateField source="updated_at" showTime />
       </Datagrid>
     </List>
   ),
