bdd1c745f7fafbedda9b853aad79739d376c4728
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-09 16:43:05 +0100

    Add create and list views for roles

--- src/resources/roles.js
+++ src/resources/roles.js
@@ -1,24 +1,32 @@
 import React from "react";
-import { Create, Datagrid, DateField, DisabledInput, Edit, List, SimpleForm, TextField } from "react-admin";
+import { Create, Datagrid, DateField, Edit, List, SimpleForm, TextField, TextInput } from "react-admin";
 
-const form = (
-  <SimpleForm>
-    <DisabledInput source="id" />
-    <DisabledInput source="created_at" />
-    <DisabledInput source="updated_at" />
-  </SimpleForm>
-);
+const form = ({ location }) => {
+  const newRecord = location.pathname.match(/create/);
+
+  return (
+    <SimpleForm>
+      {newRecord ? null : <TextField source="id" />}
+
+      <TextInput source="name" />
+
+      {newRecord ? null : <TextField source="created_at" />}
+      {newRecord ? null : <TextField source="updated_at" />}
+    </SimpleForm>
+  );
+};
 
 export default {
   name: "roles",
-  edit: (props) => <Edit {...props}>{form}</Edit>,
-  create: (props) => <Create {...props}>{form}</Create>,
+  edit: (props) => <Edit {...props}>{form(props)}</Edit>,
+  create: (props) => <Create {...props}>{form(props)}</Create>,
   list: (props) => (
     <List {...props}>
       <Datagrid rowClick="edit">
         <TextField source="id" />
-        <DateField source="created_at" />
-        <DateField source="updated_at" />
+        <TextField source="name" />
+        <DateField source="created_at" showTime />
+        <DateField source="updated_at" showTime />
       </Datagrid>
     </List>
   ),
