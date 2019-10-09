4925c033f22f965786ddbe3154feed48672f20e6
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-09 14:32:36 +0100

    Add create and list views for follow-up activities

--- src/resources/follow_up_activities.js
+++ src/resources/follow_up_activities.js
@@ -1,24 +1,45 @@
 import React from "react";
-import { Create, Datagrid, DateField, DisabledInput, Edit, List, SimpleForm, TextField } from "react-admin";
+import { Create, Datagrid, DateField, Edit, List, SimpleForm, TextField } from "react-admin";
+import { ReferenceInput, SelectInput, ReferenceField } from "react-admin";
 
-const form = (
-  <SimpleForm>
-    <DisabledInput source="id" />
-    <DisabledInput source="created_at" />
-    <DisabledInput source="updated_at" />
-  </SimpleForm>
-);
+const form = ({ location }) => {
+  const newRecord = location.pathname.match(/create/);
+
+  return (
+    <SimpleForm>
+      {newRecord ? null : <TextField source="id" />}
+
+      <ReferenceInput source="activity_id" reference="activities">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+
+      <ReferenceInput source="follow_up_activity_id" reference="activities">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+
+      {newRecord ? null : <TextField source="created_at" />}
+
+      {newRecord ? null : <TextField source="updated_at" />}
+    </SimpleForm>
+  );
+};
 
 export default {
   name: "follow_up_activities",
-  edit: (props) => <Edit {...props}>{form}</Edit>,
-  create: (props) => <Create {...props}>{form}</Create>,
+  edit: (props) => <Edit {...props}>{form(props)}</Edit>,
+  create: (props) => <Create {...props}>{form(props)}</Create>,
   list: (props) => (
     <List {...props}>
       <Datagrid rowClick="edit">
         <TextField source="id" />
-        <DateField source="created_at" />
-        <DateField source="updated_at" />
+        <ReferenceField source="activity_id" reference="activities">
+          <TextField source="name" />
+        </ReferenceField>
+        <ReferenceField source="follow_up_activity_id" reference="activities">
+          <TextField source="name" />
+        </ReferenceField>
+        <DateField source="created_at" showTime />
+        <DateField source="updated_at" showTime />
       </Datagrid>
     </List>
   ),
