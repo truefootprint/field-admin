7f7432bc6dbadda690c14b465a1694eabce1bcea
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-09 16:41:55 +0100

    Add create and list views for responses

--- src/resources/responses.js
+++ src/resources/responses.js
@@ -1,24 +1,49 @@
 import React from "react";
-import { Create, Datagrid, DateField, DisabledInput, Edit, List, SimpleForm, TextField } from "react-admin";
+import { Create, Datagrid, DateField, Edit, List, SimpleForm, TextField } from "react-admin";
+import { NumberInput, TextInput, ReferenceInput, SelectInput, ReferenceField } from "react-admin";
 
-const form = (
-  <SimpleForm>
-    <DisabledInput source="id" />
-    <DisabledInput source="created_at" />
-    <DisabledInput source="updated_at" />
-  </SimpleForm>
-);
+const form = ({ location }) => {
+  const newRecord = location.pathname.match(/create/);
+
+  return (
+    <SimpleForm>
+      {newRecord ? null : <TextField source="id" />}
+
+      <NumberInput label="Project question id" source="project_question_id" />
+
+      <ReferenceInput source="user_id" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+
+      <TextInput source="value" />
+
+      {newRecord ? null : <TextField source="created_at" />}
+      {newRecord ? null : <TextField source="updated_at" />}
+    </SimpleForm>
+  );
+};
 
 export default {
   name: "responses",
-  edit: (props) => <Edit {...props}>{form}</Edit>,
-  create: (props) => <Create {...props}>{form}</Create>,
+  edit: (props) => <Edit {...props}>{form(props)}</Edit>,
+  create: (props) => <Create {...props}>{form(props)}</Create>,
   list: (props) => (
     <List {...props}>
       <Datagrid rowClick="edit">
         <TextField source="id" />
-        <DateField source="created_at" />
-        <DateField source="updated_at" />
+
+        <ReferenceField source="project_question_id" reference="project_questions">
+          <TextField source="id" />
+        </ReferenceField>
+
+        <ReferenceField source="user_id" reference="users">
+          <TextField source="name" />
+        </ReferenceField>
+
+        <TextField source="value" />
+
+        <DateField source="created_at" showTime />
+        <DateField source="updated_at" showTime />
       </Datagrid>
     </List>
   ),
