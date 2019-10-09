4863554457106bb4884c871d5469c789bfb1b33f
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-09 11:56:25 +0100

    Add fields to the Questions resource

--- src/resources/questions.js
+++ src/resources/questions.js
@@ -1,18 +1,48 @@
 import React from "react";
 import { Create, Datagrid, DateField, DisabledInput, Edit, List, SimpleForm, TextField } from "react-admin";
+import { BooleanInput, NumberInput, RadioButtonGroupInput, ReferenceInput, SelectInput, TextInput } from "react-admin";
+import Conditional from "../components/conditional";
 
-const form = (
-  <SimpleForm>
-    <DisabledInput source="id" />
-    <DisabledInput source="created_at" />
-    <DisabledInput source="updated_at" />
-  </SimpleForm>
-);
+const form = ({ location }) => {
+  const newRecord = location.pathname.match(/create/);
+
+  return (
+    <SimpleForm>
+      {newRecord ? null : <DisabledInput source="id" />}
+
+      {newRecord ? null : <DisabledInput source="created_at" />}
+
+      {newRecord ? null : <DisabledInput source="updated_at" />}
+
+      <ReferenceInput source="type" reference="question_types">
+        <RadioButtonGroupInput />
+      </ReferenceInput>
+
+      <Conditional when={d => d.type === "FreeTextQuestion"}>
+        <NumberInput source="expected_length" />
+      </Conditional>
+
+      <Conditional when={d => d.type === "MultiChoiceQuestion"}>
+        <BooleanInput source="multiple_answers" />
+      </Conditional>
+
+      <ReferenceInput source="data_type" reference="question_data_types">
+        <RadioButtonGroupInput />
+      </ReferenceInput>
+
+      <ReferenceInput source="topic_id" reference="topics">
+        <SelectInput source="name" />
+      </ReferenceInput>
+
+      <TextInput source="text" />
+    </SimpleForm>
+  );
+};
 
 export default {
   name: "questions",
-  edit: (props) => <Edit {...props}>{form}</Edit>,
-  create: (props) => <Create {...props}>{form}</Create>,
+  edit: (props) => <Edit {...props}>{form(props)}</Edit>,
+  create: (props) => <Create {...props}>{form(props)}</Create>,
   list: (props) => (
     <List {...props}>
       <Datagrid rowClick="edit">
