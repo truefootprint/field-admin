11e144bc206ab74709b818147d38b002ad57e97d
Chanda Pwapwa <mulaikopwapwa@gmail.com>
2021-05-26 17:35:34 +0200

    changes for image support, multichoice questions

--- src/extensions/with_file_upload.js
+++ src/extensions/with_file_upload.js
@@ -1,24 +1,55 @@
 import { HOST } from "../consts"
 
-const withFileUpload = requestHandler => (type, resource, params) => {
-    if ((type === "CREATE" || type === "UPDATE") && typeof params.data.file !== "undefined") {
-        let rawFile = params.data.file.rawFile;
+  const withFileUpload = requestHandler => (type, resource, params) => {
+    console.log("WHAT IS THE TYPE?");
+    console.log(type);
+    if ((type === "CREATE" || type === "UPDATE") && typeof params.data.photo !== "undefined") {
+        let rawFile = params.data.photo.rawFile;
         let form = new FormData();
-
-        form.set("file", rawFile);
+        console.log("ON EDIT");
+        console.log(params);
+        form.set("photo", rawFile);
         form.set("name", rawFile.name);
+        form.set("order", params.data.order);
+        form.set("question_id", params.data.question_id);
+        form.set("text", params.data.text);
 
         const token = localStorage.getItem("token");
         const headers = new Headers();
 
         headers.append("Authorization", `Basic ${token}`);
+        //headers.append("Access-Control-Allow-Origin", "*");
 
+        if (type === "UPDATE"){
+          console.log("UPDATE IF");
+          return fetch(`${HOST}/${resource}/${params.data.id}`, { method: "PUT", body: form, headers: headers })
+          .then((response) => response.json())
+          .then((json) => ( { data: { id: json.id }} ))
+        } else {
         return fetch(`${HOST}/${resource}`, { method: "POST", body: form, headers: headers })
           .then((response) => response.json())
           .then((json) => ( { data: { id: json.id }} ))
+        }
     }
 
-    return requestHandler(type, resource, params);
-};
-
-export default withFileUpload;
+      if ((type === "CREATE" || type === "UPDATE") && typeof params.data.file !== "undefined") {
+          let rawFile = params.data.file.rawFile;
+          let form = new FormData();
+  
+          form.set("file", rawFile);
+          form.set("name", rawFile.name);
+  
+          const token = localStorage.getItem("token");
+          const headers = new Headers();
+  
+          headers.append("Authorization", `Basic ${token}`);
+  
+          return fetch(`${HOST}/${resource}`, { method: "POST", body: form, headers: headers })
+            .then((response) => response.json())
+            .then((json) => ( { data: { id: json.id }} ))
+      }
+  
+      return requestHandler(type, resource, params);
+  };
+  
+  export default withFileUpload;
