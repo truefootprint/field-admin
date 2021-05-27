98f05f8d29b1d01a0aa4a336c674ed5325e87419
Chanda Pwapwa <mulaikopwapwa@gmail.com>
2021-05-27 14:13:45 +0200

    Changes for image upload

--- src/extensions/with_file_upload.js
+++ src/extensions/with_file_upload.js
@@ -1,15 +1,16 @@
 import { HOST } from "../consts"
+import { v4 as uuidv4 } from 'uuid';
 
   const withFileUpload = requestHandler => (type, resource, params) => {
-    console.log("WHAT IS THE TYPE?");
-    console.log(type);
+    
     if ((type === "CREATE" || type === "UPDATE") && typeof params.data.photo !== "undefined") {
-        let rawFile = params.data.photo.rawFile;
         let form = new FormData();
-        console.log("ON EDIT");
-        console.log(params);
-        form.set("photo", rawFile);
-        form.set("name", rawFile.name);
+
+        if (params.data.photo.rawFile !== undefined){
+          let rawFile = params.data.photo.rawFile;        
+          form.set("photo", rawFile); 
+          form.set("name", rawFile.name);
+        }
         form.set("order", params.data.order);
         form.set("question_id", params.data.question_id);
         form.set("text", params.data.text);
@@ -18,15 +19,20 @@ import { HOST } from "../consts"
         const headers = new Headers();
 
         headers.append("Authorization", `Basic ${token}`);
-        //headers.append("Access-Control-Allow-Origin", "*");
-
+        headers.append("Accept-Language", params.data.locale)
+        
         if (type === "UPDATE"){
-          console.log("UPDATE IF");
           return fetch(`${HOST}/${resource}/${params.data.id}`, { method: "PUT", body: form, headers: headers })
           .then((response) => response.json())
           .then((json) => ( { data: { id: json.id }} ))
-        } else {
-        return fetch(`${HOST}/${resource}`, { method: "POST", body: form, headers: headers })
+        } 
+        if (type === "CREATE") {
+          console.log("CREATE IF");
+          for (var pair of form.entries())
+          {
+          console.log(pair[0]+ ', '+ pair[1]); 
+          }
+          return fetch(`${HOST}/${resource}`, { method: "POST", body: form, headers: headers })
           .then((response) => response.json())
           .then((json) => ( { data: { id: json.id }} ))
         }
